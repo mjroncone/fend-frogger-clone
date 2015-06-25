6dc2811e55508c2bb32e4857666af73076a2e71e
Michael Roncone <mjroncone@gmail.com>
2015-06-25 18:13:30 -0500

    style: fix spacing in code.

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ var Enemy = function() {
     var locYOptions = [55 , 137 , 220];
     this.locY = locYOptions[Math.floor(Math.random() * 3)];
     this.speed = Math.random() * 600 + 100;
-
 };
 
 // Update the enemy's position, required method for game
@@ -163,12 +162,11 @@ var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+   var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+   };
+   player.handleInput(allowedKeys[e.keyCode]);
 });
